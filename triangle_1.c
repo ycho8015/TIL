6c2f1b707824f3b213b29e1aeec0e306b3c87991
ycho8015 <ycho8015@gmail.com>
2019-09-17 16:49:32 +0900

    prints floyd's triangle

---
+++ triangle_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+  int n;
+  scanf("%d", &n);
+  int idx = 1;
+  while(n != 0) {
+    for (int i = 0; i < idx; i++) {
+      printf("*");
+    }
+    printf("\n");
+    idx++;
+    n--;
+  }
+  return 0;
+}
