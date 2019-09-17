7c307ed87e26ab955ffb5454082c7c352040980b
ycho8015 <ycho8015@gmail.com>
2019-09-17 18:14:56 +0900

    print a reversed array

---
+++ ReversedArray.c
@@ -0,0 +1,20 @@
+// program to print a reversed array
+#include <stdio.h>
+#define MAX 1000
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    int arr[MAX];
+
+    int i;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
