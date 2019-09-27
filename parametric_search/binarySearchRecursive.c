8d994f6a5af42af50ad7c70731eedaa3fa3def41
ycho8015 <ycho8015@gmail.com>
2019-09-27 14:25:55 +0900

    modified base case

--- parametric_search/binarySearchRecursive.c
+++ parametric_search/binarySearchRecursive.c
@@ -2,36 +2,33 @@
 #define MAX 100000
 
 int binarySearch(int arr[], int low, int high, int value) {
-    if (low > high) {
+    if (low > high) 
         return -1;
-    } else if (low == high) {
-        if (arr[low] == value) {
-            return low;
-        } else {
-            return -1;  
-        }
-    } else {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == value) {
-            return mid;
-        } else if (arr[mid] > value) {
-            return binarySearch(arr, low, mid - 1, value);
-        } else {
-            return binarySearch(arr, mid + 1, high, value);
-        }
-    }
+    
+    int mid = low + (high - low) / 2;
+    if (arr[mid] == value)
+        return mid;
+    else if (arr[mid] > value)
+        return binarySearch(arr, low, mid - 1, value);
+    else
+        return binarySearch(arr, mid + 1, high, value);
 }
 
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
-
+    
     int arr[MAX];
-    for (int i = 0; i < n; i++) {
+    int i;
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
-    printf("%d", binarySearch(arr, 0, n - 1, k));
-
+    
+    int num;
+    for (i = 0; i < k; i++) {
+        scanf("%d", &num);
+        printf("%d\n", binarySearch(arr, 0, n - 1, num));
+    }
+    
     return 0;
 }
